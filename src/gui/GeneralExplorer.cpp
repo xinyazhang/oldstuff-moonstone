e5aa27a87571cb574a3fd3798e31176fb2662be5
zxy_thf <zxy_thf@hotmail.com>
2011-05-10 23:03:12 +0800

    gui: UI support for drop.

--- src/gui/GeneralExplorer.cpp
+++ src/gui/GeneralExplorer.cpp
@@ -10,6 +10,9 @@ GeneralExplorer::GeneralExplorer()
 			this, SLOT(launch_search()));
 	ui_->result_tree->setSelectionMode(QAbstractItemView::ExtendedSelection);
 	ui_->result_tree->setDragEnabled(true);
+	/* used for debug */
+	ui_->result_tree->setAcceptDrops(true);
+	ui_->result_tree->setDropIndicatorShown(true);
 }
 
 void GeneralExplorer::install_sh(SearchHandler* sh)
