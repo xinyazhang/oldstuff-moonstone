d0d333f689d4f355aed5db87dc84d5e30583ec42
zxy <zxy_thf@hotmail.com>
2011-08-19 19:39:57 +0800

    pal: add filestream abstraction.

--- src/pal/stdtype.h
+++ src/pal/stdtype.h
@@ -9,5 +9,19 @@ using boost::shared_ptr;
 typedef int64_t idx_t;
 #define USE_INT64_INDEX
 
+#ifndef UNICODE_SELECTED
+#error "Please choose UTF-16LE or UTF-8
+#else
+
+#ifdef USE_UTF16LE
+#include <fstream>
+typedef std::wfstream filestream;
+#endif
+
+#ifdef USE_UTF8
+#include <fstream>
+typedef std::fstream filestream;
+#endif
+#endif
 
 #endif
