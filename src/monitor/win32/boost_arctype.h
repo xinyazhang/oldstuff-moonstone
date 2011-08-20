ee713d0e371e80f1c999d17fb29ce8552091a67b
zxy <zxy_thf@hotmail.com>
2011-08-20 17:55:32 +0800

    monitor: use text_w?archive instead of xml_w?archive to avoid bugs.

--- src/monitor/win32/boost_arctype.h
+++ src/monitor/win32/boost_arctype.h
@@ -1,10 +1,10 @@
 #ifndef BOOST_ARCTYPE_H
 #define BOOST_ARCTYPE_H
 
-#include <boost/archive/xml_wiarchive.hpp>
-#include <boost/archive/xml_woarchive.hpp>
+#include <boost/archive/text_wiarchive.hpp>
+#include <boost/archive/text_woarchive.hpp>
 
-typedef boost::archive::xml_wiarchive barc_i; // Boost ARChive Input
-typedef boost::archive::xml_woarchive barc_o; // Boost ARChive Output
+typedef boost::archive::text_wiarchive barc_i; // Boost ARChive Input
+typedef boost::archive::text_woarchive barc_o; // Boost ARChive Output
 
 #endif
