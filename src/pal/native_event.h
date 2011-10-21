16c2a88b617e091916140bf27c21450023c8fdc5
Xinya ZHANG <zxy_thf@hotmail.com>
2011-10-21 23:34:27 +0800

    pal: native_event interface 	- CreateEvent under Win32 	- eventfd under Linux

---
+++ src/pal/native_event.h
@@ -0,0 +1,21 @@
+#ifndef NATIVE_EVENT_H
+#define NATIVE_EVENT_H
+
+/*
+ * Triggered/Nontriggered Event object
+ * 	Must can be wait/select/etc.
+ *
+ * For win32, it is Event
+ * For linux, it is event_fd
+ */
+
+#include <stdtype.h>
+
+native_fd create_event(bool initvalue); // for platform uniform, must be AUTO UNFLAG
+void close_event(native_fd);
+void wait_event(native_fd);
+void flag_event(native_fd);
+
+/* NO WAIT */
+
+#endif
