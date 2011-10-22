dc642205230f1a8e1430f22265058ec219b4c2e9
Xinya ZHANG <zxy_thf@hotmail.com>
2011-10-22 16:20:40 +0800

    everything: indent clean up

--- src/everything/searchfiledbmodel.h
+++ src/everything/searchfiledbmodel.h
@@ -6,28 +6,28 @@
 class SearchFileDBModel : public QAbstractItemModel
 {
 public:
-    SearchFileDBModel(class Preferences* pref);
+	SearchFileDBModel(class Preferences* pref);
 
-    QVariant data(const QModelIndex &index, int role) const;
-    Qt::ItemFlags flags(const QModelIndex &index) const;
-    QVariant headerData(int section,
-                    Qt::Orientation orientation,
-                    int role = Qt::DisplayRole) const;
-    QModelIndex index(int row,
-                    int column,
-                    const QModelIndex &parent = QModelIndex()) const;
-    QModelIndex parent(const QModelIndex &index) const;
-    int rowCount(const QModelIndex &parent = QModelIndex()) const;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const;
+	QVariant data(const QModelIndex &index, int role) const;
+	Qt::ItemFlags flags(const QModelIndex &index) const;
+	QVariant headerData(int section,
+			Qt::Orientation orientation,
+			int role = Qt::DisplayRole) const;
+	QModelIndex index(int row,
+			int column,
+			const QModelIndex &parent = QModelIndex()) const;
+	QModelIndex parent(const QModelIndex &index) const;
+	int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	int columnCount(const QModelIndex &parent = QModelIndex()) const;
 
-    int change_searching_text(const QString& str);
+	int change_searching_text(const QString& str);
 
-    static int EventCallback(void* /*SearchFileDBModel*, ENGINE_EVENT*/, ENGINE_EVENT, void*);
-    bool event ( QEvent * e );
-    void update_result();
+	static int EventCallback(void* /*SearchFileDBModel*, ENGINE_EVENT*/, ENGINE_EVENT, void*);
+	bool event ( QEvent * e );
+	void update_result();
 private:
-    class search_service *se_cookie;
-    class Preferences* pref;
+	class search_service *se_cookie;
+	class Preferences* pref;
 };
 
 #endif // SEARCHFILEDBMODEL_H
