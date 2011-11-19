c5120e4c4101fe66765f9dc2d5515b58595f0538
Xinya ZHANG <zxy_thf@hotmail.com>
2011-11-19 23:48:03 +0800

    kernel: index engine Ver. 2, a new index engine based fdpool and tasklet queue

---
+++ src/kernel/index_engine2.cc
@@ -0,0 +1,43 @@
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <pal/volume.h>
+#include <pal/native_event.h>
+#include "index_engine.h"
+#include <boost/thread.hpp>
+#include <boost/thread/locks.hpp>
+#include <boost/thread/mutex.hpp>
+#include "watching.h"
+#include "factory.h"
+#include "vfp_queue.h"
+
+index_engine_t::index_engine_t(Database* dbmgr)
+	:dbmgr_(dbmgr)
+{
+	fdpool_ = factory::fdpool();
+	prep_queue_ = factory::tasklet_queue();
+}
+
+index_engine_t::~index_engine_t()
+{
+	delete prep_queue_;
+	delete fdpool_;
+}
+
+bool index_engine_t::queue_volume(const struct volume& vol)
+{
+	return prep_queue_->enqueue(
+			factory::volpreplet(VOLFDPREP_ADD, vol, fdpool_, dbmgr_));
+}
+
+bool index_engine_t::remove_volume(const struct volume& vol)
+{
+	return prep_queue_->enqueue(
+			factory::volpreplet(VOLFDPREP_RMV, vol, fdpool_, dbmgr_));
+}
+
+std::vector<volume> index_engine_t::volume_list() const
+{
+	return stat::watching_volumes();
+}
+
