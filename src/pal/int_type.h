4455766a86d369f23e5eea49ed1c6ae475c730fd
Xinya ZHANG <zxy_thf@hotmail.com>
2011-11-19 23:51:59 +0800

    pal: add u32 and u64 for simplify

--- src/pal/int_type.h
+++ src/pal/int_type.h
@@ -3,6 +3,11 @@
 
 #include <stdint.h> // as we're using VC10, it's unnecessary to define our own stdint.h
 
+typedef uint32_t u32;
+typedef int32_t i32;
+typedef uint64_t u64;
+typedef int64_t i64;
+
 #ifdef _WIN32
 #define EXPORT_TO_DLL __declspec(dllexport)
 #endif
