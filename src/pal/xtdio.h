f0541f742c5a11a78b22c6b83bc6c664e7088eb8
Xinya ZHANG <zxy_thf@hotmail.com>
2011-12-10 21:22:58 +0800

    pal/xtdio: cross(X) platform stdio functions

---
+++ src/pal/xtdio.h
@@ -0,0 +1,18 @@
+#ifndef XTDIO_H
+#define XTDIO_H
+
+#include "unicode.h"
+
+#ifdef _MSC_VER
+#define xvsnprintf _vsnwprintf
+#define xfprintf _wfprintf
+#define XTDIO_WRITE_TEXT_FLAG L"ab+"
+#define xfopen(x, y) _wfopen(UT(x), (y))
+#else
+#define xvsnprintf vsnprintf
+#define xprintf fprintf
+#define XTDIO_WRITE_TEXT_FLAG "a+"
+#define xfopen(x,y) fopen((x), (y))
+#endif
+
+#endif
