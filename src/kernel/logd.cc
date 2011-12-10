0c951a2e35218b931bd68fac786adff952851496
Xinya ZHANG <zxy_thf@hotmail.com>
2011-12-10 21:22:19 +0800

    kernel/logd: dump feedback_events in log to files

---
+++ src/kernel/logd.cc
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pal/xtdio.h>
+#include "feedback.h"
+#include "logd.h"
+
+logd::logd()
+{
+	journal = xfopen("LOG.TXT", XTDIO_WRITE_TEXT_FLAG);
+	if (!journal) {
+		log().printf(LOG_ERROR, "COULD NOT OPEN LOG FILE\n");
+	}
+	if (ftell(journal) == 0) {
+		fwrite("\xFF\xFE", 2, 1, journal);
+	}
+	tg_ = globaltp()->allocate_threads(1, this);
+}
+
+logd::~logd()
+{
+	log().printf(TASKLET_QUIT, "");
+	delete tg_;
+}
+
+int logd::tp_working()
+{
+	while(true) {
+		log_event le = log.next();
+		if (le.evid >= LOG_FATAL && le.evid <= LOG_DEBUG) {
+			/* print immediately */
+			xfprintf(journal, UT("%s"), (unichar*)le.payload.get());
+		}
+	}
+}
---
+++ src/kernel/logd.h
@@ -0,0 +1,24 @@
+/*
+ * Log ``daemon'' which stores message of ring buffer(maybe)
+ * to file
+ */
+#ifndef LOGD_H
+#define LOGD_H
+
+#include <pal/int_type.h>
+#include "threadpool.h"
+
+class EXPORT_TO_DLL logd
+	:public threadpool_worker_t
+{
+public:
+	logd();
+	~logd();
+
+	int tp_working();
+private:
+	threadgroup_t* tg_;
+	struct FILE* journal;
+};
+
+#endif
