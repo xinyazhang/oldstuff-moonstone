f17541b6fb76784cd41724d25fd44294252fc143
Xinya ZHANG <zxy_thf@hotmail.com>
2011-12-17 18:23:09 +0800

    gui: QFeedbackEvent

---
+++ src/everything/QFeedbackEvent.h
@@ -0,0 +1,22 @@
+#ifndef QFEEDBACK_EVENT_H
+#define QFEEDBACK_EVENT_H
+
+#include <QtCore/QEvent>
+#include <kernel/feedback.h>
+
+class QFeedbackEvent
+	:public QEvent
+{
+public:
+	QFeedbackEvent(feedback_event& ev)
+		:ev_(ev), QEvent(ev.evid + QEvent::User)
+	{
+	}
+
+	~QFeedbackEvent() { }
+	feedback_event& ev() { return ev_; }
+private:
+	feedback_event ev_;
+}
+
+#endif
