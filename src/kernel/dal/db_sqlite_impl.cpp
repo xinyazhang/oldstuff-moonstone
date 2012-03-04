43caed2d41c94604ef8783c0be10bfc3c6673ae9
Xinya ZHANG <zxy_thf@hotmail.com>
2012-03-04 16:43:51 +0800

    Machine id started from 1

--- src/kernel/dal/db_sqlite_impl.cpp
+++ src/kernel/dal/db_sqlite_impl.cpp
@@ -94,7 +94,7 @@ static const unistr sql_template[] =
 		UT("machine_list(")\
 		UT("id INTEGER PRIMARY KEY ASC AUTOINCREMENT")\
 		UT(", name TEXT, comment TEXT);"),
-	UT("INSERT INTO machine_list(0, \"localhost\");"),
+	UT("INSERT INTO machine_list(1, \"localhost\");"),
 	UT("CREATE TABLE IF NOT EXISTS known_vols(id INTEGER PRIMARY KEY ASC AUTOINCREMENT, uuid TEXT UNIQUE, status INTEGER, filesystem INTEGER, vollabel TEXT);"),
 	UT("CREATE TABLE IF NOT EXISTS known_dentry(volid INTEGER REFERENCES known_vols(id), inode INTEGER, parent INTEGER, name TEXT, checked INTEGER, PRIMARY KEY(volid, parent, name));"),
 	UT("CREATE TABLE IF NOT EXISTS known_file(volid INTERGER REFERENCES known_vols(id), inode INTEGER, refc INTEGER, PRIMARY KEY(volid, inode));"),
