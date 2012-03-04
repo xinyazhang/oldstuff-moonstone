13adf71312004e3db7c5925511b2d1ba1361935c
Xinya ZHANG <zxy_thf@hotmail.com>
2012-03-04 22:02:16 +0800

    machine model items' interfaces

---
+++ src/everything/machinemodelitem.h
@@ -0,0 +1,47 @@
+#ifndef MACHINE_MODEL_ITEM_H
+#define MACHINE_MODEL_ITEM_H
+
+#include "treeitem.h"
+
+struct machine_t;
+struct access_point;
+struct volume;
+
+class MachineRoot
+	:public TreeItem
+{
+public:
+	MachineRoot();
+	~MachineRoot();
+protected:
+	/* To create new MachineItem */
+	virtual TreeItem* spawnChild(int position, int columns);
+	/* MachineRoot doesn't have a backend */
+};
+
+class MachineItem
+	:public TreeItem
+{
+public:
+	MachineItem(const machine_t&);
+	~MachineItem();
+protected:
+	/* To create new APItem */
+	virtual TreeItem* spawnChild(int position, int columns);
+	virtual int removeAtBackend();
+	virtual int addAtBackend();
+	virtual int updateAtBackend():
+};
+
+class APItem
+	:public TreeItem
+{
+public:
+	APItem(MachineItem*, const access_point&);
+	~APItem();
+protected:
+	/* return NULL as no children for APs */
+	virtual TreeItem* spawnChild(int position, int columns) { return NULL; }
+};
+
+#endif
