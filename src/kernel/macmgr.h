01372794331ee1f4c7066bf579eb34c828f77b1a
Xinya ZHANG <zxy_thf@hotmail.com>
2012-03-05 13:42:14 +0800

    refine macmgr_t's interface

--- src/kernel/macmgr.h
+++ src/kernel/macmgr.h
@@ -2,26 +2,28 @@
 #define MACHINE_MANAGER_H
 
 #include <pal/stdtype.h>
-struct machine_t;
-struct access_point;
+class machine_t;
+class access_point;
 
 class EXPORT_TO_DLL macmgr_t
 {
 public:
 	macmgr_t(class Database*);
 
-	void add_machine(const machine_t&);
+	void add_machine(machine_t&); /* would change machine_t::id */
+	void update_machine(const machine_t&);
 	void rm_machine(const machine_t&);
 
-	void add_ap(const access_point&);
+	void add_ap(access_point&);
+	void update_ap(const access_point&);
 	void rm_ap(const access_point&);
 
-	int64_t macnum();
+	int macnum();
 	machine_t macfirst();
 	machine_t macthis(idx_t);
 	machine_t macnext(const machint_t&);
 
-	int64_t apnum(const machine_t&);
+	int apnum(const machine_t&);
 	access_point apfirst(const machine_t&);
 	access_point apthis(idx_t);
 	access_point apnext(const access_point&);
